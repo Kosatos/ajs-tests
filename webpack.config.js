24a091b5290537c18caca93f6c3b994217a0f0cd
Konstantin Muzyka <kosatkinms@mail.ru>
2022-01-30 22:10:31 +0300

    change entry point, tunned webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
+  entry: './src/js/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
   },
@@ -31,14 +32,4 @@ module.exports = {
       },
     ],
   },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: './src/index.html',
-      filename: './index.html',
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[id].css',
-    }),
-  ],
 };
